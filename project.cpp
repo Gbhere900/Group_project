467f2e9405d6ea6dc91d5291477fbf1858759f97
ZhuZhaoMing <1843123351@qq.com>
2024-11-26 22:57:32 +0800

    进行了一些声明，包括一些变量和结构体的命名

--- project.cpp
+++ project.cpp
@@ -1,7 +1,21 @@
-#include<stdio.h>
-using namespace std
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<time.h>
+
+int A = 1;
+int x = 0, y = 0;
+int mouse_x = 0, mouse_y = 0;		//老鼠坐标
+int granary_x = 0, granary_y = 0;		//粮仓坐标
+
+
+int maze[9][9];    //地图最大尺寸
+
+struct best                  //用于记录最短路径的结构体
 {
-	cout << "hello world" << endl;
-	return 0;
-}
+	int min;
+	int b[9][9];
+}Best;
+
+int route_number;		//正确路线个数
+
